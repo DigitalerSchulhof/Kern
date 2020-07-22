e930434a8dcf898f2b946f9f64e5e8033b96a360
jeengbe <jesper.engberg@gmx.at>
2020-07-22 16:25:36 +0200

    Seitenladebalken

--- seiten/fehler/404.php
+++ seiten/fehler/404.php
@@ -2,9 +2,5 @@
 $DSH_TITEL = "Fehler 404";
 $CODE .= new Kern\Aktionszeile(true, false);
 
-
-$spalte = new UI\Spalte(new UI\Meldung("Fehler 404", "Die Seite konnte nicht gefunden werden!", "Fehler"));
-$zeile = new UI\Zeile($spalte);
-
-$CODE .= $zeile;
+$CODE .= new UI\Zeile(new UI\Spalte(null, new UI\Meldung("Fehler 404", "Die Seite konnte nicht gefunden werden!", "Fehler")));
 ?>
