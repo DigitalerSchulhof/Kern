0d1b40d47b196e9b0da24e2e48aeec05d6c0b283
jeengbe <jesper.engberg@gmx.at>
2020-07-24 20:10:51 +0200

    Überschriften in css

--- seiten/fehler/404.php
+++ seiten/fehler/404.php
@@ -2,6 +2,6 @@
 $DSH_TITEL = "Fehler 404";
 $CODE .= new Kern\Aktionszeile(true, false);
 
-$CODE .= new UI\Zeile(new UI\Spalte(null, new UI\Meldung("Fehler 404", "Die Seite konnte nicht gefunden werden!", "Fehler")));
+$CODE .= UI\Zeile::standard(new UI\Meldung("Fehler 404", "Die Seite konnte nicht gefunden werden!", "Fehler"));
 
 ?>
