7162788400d5d8ad509deb9b8bc5860f0d84610e
Patrick Wagner <pawa@matinf.de>
2020-08-01 09:41:00 +0200

    [NEU] Fenster, JSON-Standrad für Rückgabe

--- js/schulhof/anmeldung.js
+++ js/schulhof/anmeldung.js
@@ -93,8 +93,6 @@ kern.schulhof.anmeldung = {
     var benutzer = $("#dshAnmeldungBenutzer").getWert();
     var passwort = $("#dshAnmeldungPasswort").getWert();
 
-    core.ajax("Kern", 0, ["Anmeldung", "Anmeldedaten werden überprüft"], {benutzer: benutzer, passwort: passwort}).then((r) => {
-      core.anfrageAuswerten(r);
-    });
+    core.ajax("Kern", 0, ["Anmeldung", "Anmeldedaten werden überprüft"], {benutzer: benutzer, passwort: passwort});
   }
 };
