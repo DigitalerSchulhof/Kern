25e804aa660c2d9e93dddc926e7f03481a7424a2
jeengbe <jesper.engberg@gmx.at>
2020-08-04 11:01:31 +0200

    [Fix] Abmelden mit Meldung-Knöpfe Array

--- js/schulhof/nutzerkonto.js
+++ js/schulhof/nutzerkonto.js
@@ -6,7 +6,7 @@ kern.schulhof.nutzerkonto = {
   },
   abmelden: {
     fragen: () => {
-      core.ajax("UI", 1, ["Abmeldung", "Bitte warten"], {art: "Warnung", titel: "Wirklich abmelden?", inhalt: "Damit die Ameldung erfolgen kann ist eine Bestätigung notwendig!", aktionen: 2, knopfinhalt0: "Abmelden", knopfart0: "Warnung", knopfziel0: "kern.schulhof.nutzerkonto.abmelden.ausfuehren()", knopfinhalt1: "Abbrechen", knopfziel1: "ui.laden.aus()"});
+      core.ajax("UI", 1, ["Abmeldung", "Bitte warten"], {art: "Warnung", titel: "Wirklich abmelden?", inhalt: "Damit die Ameldung erfolgen kann ist eine Bestätigung notwendig!", aktionen: [{inhalt: "Abmelden", art: "Warnung", ziel: "kern.schulhof.nutzerkonto.abmelden.ausfuehren()"}, {inhalt: "Abbrechen", ziel: "ui.laden.aus()"}]});
     },
     ausfuehren: () => {
       core.ajax("Kern", 1, ["Abmeldung", "Die Abmeldung wird durchgeführt"], null);
