e83104e1dbaff152599ade6d8dbe887634a46faa
Patrick Wagner <pawa@matinf.de>
2020-08-17 20:29:25 +0200

    [NEU] Personenfunktionen nur Anzeigen, wenn Modul Gruppen vorhanden

--- seiten/schulhof/verwaltung/personen/personen.php
+++ seiten/schulhof/verwaltung/personen/personen.php
@@ -24,12 +24,15 @@ if ($DSH_BENUTZER->hatRecht("kern.personen.importieren.[|konten,ids]")) {
   $knopf      ->addFunktion("onclick", "kern.personen.importieren.auswahl()");
   $knoepfe[]   = $knopf;
 }
-if ($DSH_BENUTZER->hatRecht("kern.personen.kurszuordnung.[|ausDatei,ausKlasse,zuruecksetzen]")) {
+
+$vModule = array_keys($DSH_ALLEMODULE);
+
+if ($DSH_BENUTZER->hatRecht("kern.personen.kurszuordnung.[|ausDatei,ausKlasse,zuruecksetzen]") && in_array("Gruppen", $vModule)) {
   $knopf      = new UI\IconKnopf(new UI\Icon ("fas fa-chalkboard"), "Kurszuordnung");
   $knopf      ->addFunktion("onclick", "kern.personen.kurszuordnung.auswahl()");
   $knoepfe[]   = $knopf;
 }
-if ($DSH_BENUTZER->hatRecht("kern.personen.löschen.person")) {
+if ($DSH_BENUTZER->hatRecht("kern.personen.löschen.person") && in_array("Gruppen", $vModule)) {
   $knopf      = new UI\IconKnopf(new UI\Icon (UI\Konstanten::LOESCHEN), "Nicht zugeordnete Personen löschen", "Warnung");
   $knopf      ->addFunktion("onclick", "kern.personen.loeschen.nichtzugeornet.fragen()");
   $knoepfe[]   = $knopf;
