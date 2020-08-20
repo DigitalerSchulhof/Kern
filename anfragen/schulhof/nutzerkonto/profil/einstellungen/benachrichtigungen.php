c355d7bbcd2f2a21e301adc72cde2592c77d72c2
Patrick Wagner <pawa@matinf.de>
2020-08-20 19:43:33 +0200

    [NEU] Benachrichtigungen -> Notifikationen Einstellungen für Nachrichten wandern in das Modul »Postfach«

--- anfragen/schulhof/nutzerkonto/profil/einstellungen/benachrichtigungen.php
+++ anfragen/schulhof/nutzerkonto/profil/einstellungen/benachrichtigungen.php
@@ -1,11 +1,11 @@
 <?php
-Anfrage::post("id", "nachrichten", "notifikationen", "blog", "termin", "galerie");
+Anfrage::post("id", "notifikationen", "blog", "termin", "galerie");
 
 if(!Kern\Check::angemeldet()) {
   Anfrage::addFehler(-2, true);
 }
 
-if(!UI\Check::istZahl($id,0) || !UI\Check::istToggle($nachrichten) || !UI\Check::istToggle($notifikationen) || !UI\Check::istToggle($blog) || !UI\Check::istToggle($termin) || !UI\Check::istToggle($galerie)) {
+if(!UI\Check::istZahl($id,0) || !UI\Check::istToggle($notifikationen) || !UI\Check::istToggle($blog) || !UI\Check::istToggle($termin) || !UI\Check::istToggle($galerie)) {
   Anfrage::addFehler(-3, true);
 }
 
@@ -15,6 +15,6 @@ if (!$DSH_BENUTZER->hatRecht("$recht.einstellungen.benachrichtigungen")) {
   Anfrage::addFehler(-4, true);
 }
 
-$sql = "UPDATE kern_nutzereinstellungen SET notifikationsmail = [?], postmail = [?], oeffentlichertermin = [?], oeffentlicherblog = [?], oeffentlichegalerie = [?] WHERE person = ?";
+$sql = "UPDATE kern_nutzereinstellungen SET notifikationsmail = [?], oeffentlichertermin = [?], oeffentlicherblog = [?], oeffentlichegalerie = [?] WHERE person = ?";
 $anfrage = $DBS->anfrage($sql, "sssssi", $notifikationen, $nachrichten, $termin, $blog, $galerie, $id);
 ?>
